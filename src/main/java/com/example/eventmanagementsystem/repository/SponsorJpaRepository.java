83f8e70594c18a79478fddfef536d40353a9605c
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-16 09:06:53 +0530

    Update SponsorJpaRepository.java

--- src/main/java/com/example/eventmanagementsystem/repository/SponsorJpaRepository.java
+++ src/main/java/com/example/eventmanagementsystem/repository/SponsorJpaRepository.java
@@ -4,9 +4,9 @@ import com.example.eventmanagementsystem.model.Sponsor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.stereotype. Repository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface SponsorJpaRepository extends JpaRepository <Sponsor, Integer> {
+public interface SponsorJpaRepository extends JpaRepository<Sponsor, Integer> {
 }
