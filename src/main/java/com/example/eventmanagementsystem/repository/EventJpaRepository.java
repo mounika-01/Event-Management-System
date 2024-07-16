d69c2298e3c634f1cef75ea17dd564454ead56f3
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-16 09:05:45 +0530

    Update EventJpaRepository.java

--- src/main/java/com/example/eventmanagementsystem/repository/EventJpaRepository.java
+++ src/main/java/com/example/eventmanagementsystem/repository/EventJpaRepository.java
@@ -1,13 +1,60 @@
-package com.example.eventmanagementsystem.repository;
+package com.example.eventmanagementsystem.model;
 
-import com.example.eventmanagementsystem.model.Event;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import javax.persistence.*;
+import java.util.List;
 
-import org.springframework.stereotype.Repository;
+@Entity
+@Table(name = "sponsor")
+public class Sponsor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int sponsorId;
 
-@Repository
+    @Column(name = "name")
+    private String sponsorName;
+    @Column(name = "industry")
+    private String industry;
 
-public interface EventJpaRepository extends JpaRepository<Event, Integer> {
+    @ManyToMany
+    @JoinTable(name = "event_sponsor", joinColumns = @JoinColumn(name = "sponsorid"), inverseJoinColumns = @JoinColumn(name = "eventid"))
+    @JsonIgnoreProperties("sponsors")
+    private List<Event> events;
 
+    public Sponsor() {
+    }
+
+    public int getSponsorId() {
+        return sponsorId;
+    }
+
+    public void setSponsorId(int sponsorId) {
+        this.sponsorId = sponsorId;
+    }
+
+    public String getSponsorName() {
+        return sponsorName;
+    }
+
+    public void setSponsorName(String sponsorName) {
+        this.sponsorName = sponsorName;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
 }
