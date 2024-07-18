9153f08fceab6984f996b17f9091dccb82a638ef
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-18 12:12:10 +0530

    Update EventJpaRepository.java

--- src/main/java/com/example/eventmanagementsystem/repository/EventJpaRepository.java
+++ src/main/java/com/example/eventmanagementsystem/repository/EventJpaRepository.java
@@ -1,60 +1,9 @@
-package com.example.eventmanagementsystem.model;
+package com.example.eventmanagementsystem.repository;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.example.eventmanagementsystem.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name = "sponsor")
-public class Sponsor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int sponsorId;
-
-    @Column(name = "name")
-    private String sponsorName;
-    @Column(name = "industry")
-    private String industry;
-
-    @ManyToMany
-    @JoinTable(name = "event_sponsor", joinColumns = @JoinColumn(name = "sponsorid"), inverseJoinColumns = @JoinColumn(name = "eventid"))
-    @JsonIgnoreProperties("sponsors")
-    private List<Event> events;
-
-    public Sponsor() {
-    }
-
-    public int getSponsorId() {
-        return sponsorId;
-    }
-
-    public void setSponsorId(int sponsorId) {
-        this.sponsorId = sponsorId;
-    }
-
-    public String getSponsorName() {
-        return sponsorName;
-    }
-
-    public void setSponsorName(String sponsorName) {
-        this.sponsorName = sponsorName;
-    }
-
-    public String getIndustry() {
-        return industry;
-    }
-
-    public void setIndustry(String industry) {
-        this.industry = industry;
-    }
-
-    public List<Event> getEvents() {
-        return events;
-    }
-
-    public void setEvents(List<Event> events) {
-        this.events = events;
-    }
+@Repository
+public interface EventJpaRepository extends JpaRepository<Event, Integer> {
 }
